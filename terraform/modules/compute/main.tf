fd834c7ece0f16e83830bd7f58eb1d1888872b9b
Nader Hadad Souza <naderhadadsouza@gmail.com>
2023-12-06 20:47:35 -0300

    limpa

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -42,6 +42,7 @@ data "template_file" "user_data" {
 }
 
 resource "aws_instance" "instance" {
+    count                  = 1
     ami                    = "${var.ami}"
     instance_type          = "t2.micro"
     subnet_id              = "${var.subnet_id}"
